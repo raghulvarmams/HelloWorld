83c718fa6a00d0a7991930608fa66a3f0e5c4fb2
raghulvarma <raghulvarma@gmail.com>
2024-11-01 08:53:24 +0530

    Greet User functionality

--- HelloWorldJenkinsGit/Program.cs
+++ HelloWorldJenkinsGit/Program.cs
@@ -1,3 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+Console.WriteLine("Enter User Name");
+string userName = Console.ReadLine();
+Console.WriteLine("Hello, " + userName);
 Console.ReadLine();
